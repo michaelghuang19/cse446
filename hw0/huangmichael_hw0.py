e7e016740fd34da6a99373cb4ab3990487f79b20
Michael Huang <mhuang19@cs.washington.edu>
2021-03-31 19:40:58 -0700

    completed the easy linalg

--- hw0/huangmichael_hw0.py
+++ hw0/huangmichael_hw0.py
@@ -3,10 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def main():
-  # A.11 a.
+def matrix_compute():
+  # A.11 a. A^-1
+  print(A.11.a)
 
-  # A.11 b: A^-1b, Ac
+  # A.11 b. A^-1b, Ac
+  print(A.11.b)
+
+
+# Use Z=numpy.random.randn(n) to generate the random variables;
+# plt.step(sorted(Z), np.arange(1,n+1)/float(n)) to plot
+def plot_rvs():
+  print(A.12.a)
+
+  print(A.12.b)
+
+
+def main():
+  matrix_compute()
+  plot_rvs()
 
 if __name__ == "__main__":
   main()
