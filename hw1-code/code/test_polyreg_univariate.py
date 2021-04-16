947f4f5c96bb318c41e789382cb597540add5ff8
Michael Huang <mhuang19@cs.washington.edu>
2021-04-15 23:44:53 -0700

    refactored code and figure display for 4

--- hw1-code/code/test_polyreg_univariate.py
+++ hw1-code/code/test_polyreg_univariate.py
@@ -7,6 +7,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from polyreg import PolynomialRegression
 
+def plotUnivariate(X, y, d, reg_lambda):
+    
+    # minX = 3
+    # maxY = max(errorTest[minX+1:])
+
+    # xs = np.arange(len(errorTrain))
+    # plt.plot(xs, errorTrain, 'r-o')
+    # plt.plot(xs, errorTest, 'b-o')
+    # plt.plot(xs, np.ones(len(xs)), 'k--')
+    # plt.legend(['Training Error', 'Testing Error'], loc='best')
+    # plt.title('Learning Curve (d='+str(degree)+', lambda='+str(regLambda)+')')
+    # plt.xlabel('Training samples')
+    # plt.ylabel('Error')
+    # plt.yscale('log')
+    # plt.ylim(top=maxY)
+    # plt.xlim((minX, 10))
+
+    model = PolynomialRegression(degree=d, reg_lambda=reg_lambda)
+    model.fit(X, y)
+
+    # output predictions
+    xpoints = np.linspace(np.max(X), np.min(X), 100).reshape(-1, 1)
+    ypoints = model.predict(xpoints)
+
+    # plot curve
+    plt.plot(X, y, 'rx')
+    plt.title('PolyRegression with d = ' + str(d) + ', lambda = ' + str(reg_lambda))
+    plt.plot(xpoints, ypoints, 'b-')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+
 if __name__ == "__main__":
     '''
         Main function to test polynomial regression
@@ -22,22 +53,32 @@ if __name__ == "__main__":
 
     # regression with degree = d
     d = 8
-    reg_lambda = 1E-1
-    model = PolynomialRegression(degree=d, reg_lambda=reg_lambda)
-    model.fit(X, y)
+    reg_lambda = 0.5
 
-    # output predictions
-    xpoints = np.linspace(np.max(X), np.min(X), 100).reshape(-1, 1)
-    ypoints = model.predict(xpoints)
+    # model = PolynomialRegression(degree=d, reg_lambda=reg_lambda)
+    # model.fit(X, y)
 
-    # plot curve
-    plt.figure()
-    plt.plot(X, y, 'rx')
-    plt.title('PolyRegression with d = '+str(d))
-    plt.plot(xpoints, ypoints, 'b-')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
+    # # output predictions
+    # xpoints = np.linspace(np.max(X), np.min(X), 100).reshape(-1, 1)
+    # ypoints = model.predict(xpoints)
 
-    # show isn't working for me for some reason
-    plt.savefig("a4_" + str(reg_lambda) + ".png")
+    # # plot curve
+    # plt.figure()
+    # plt.plot(X, y, 'rx')
+    # plt.title('PolyRegression with d = ' + str(d) + ', lambda = ' + str(reg_lambda))
+    # plt.plot(xpoints, ypoints, 'b-')
+    # plt.xlabel('X')
+    # plt.ylabel('Y')
+    # plt.show()
+
+    # # show isn't working for me for some reason
+    # plt.savefig("a4_" + str(reg_lambda) + ".png")
+
+    reg_lambda_list = [0, 1E-8, 1E-6, 1E-4, 1E-2, 1E-1, 1, 100]
+    plt.figure(figsize=(20, 10), dpi=100)
+
+    for idx, reg_lambda in enumerate(reg_lambda_list):
+        plt.subplot(2, 4, idx + 1)
+        plotUnivariate(X, y, d, reg_lambda)
+    
+    plt.savefig("a4_compilation.png")
