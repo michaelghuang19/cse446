e8f3f5ee1b7ef2423815b47e22d1856516623cee
Michael Huang <mhuang19@cs.washington.edu>
2021-04-18 20:29:17 -0700

    clarified single vs multi plot

--- hw1-code/code/test_polyreg_univariate.py
+++ hw1-code/code/test_polyreg_univariate.py
@@ -7,6 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from polyreg import PolynomialRegression
 
+single_plot = True
+
 def plotUnivariate(X, y, d, reg_lambda):
     
     # minX = 3
@@ -53,32 +55,35 @@ if __name__ == "__main__":
 
     # regression with degree = d
     d = 8
-    reg_lambda = 0.5
+    reg_lambda = 0
 
-    # model = PolynomialRegression(degree=d, reg_lambda=reg_lambda)
-    # model.fit(X, y)
+    if single_plot:
+        model = PolynomialRegression(degree=d, reg_lambda=reg_lambda)
+        model.fit(X, y)
 
-    # # output predictions
-    # xpoints = np.linspace(np.max(X), np.min(X), 100).reshape(-1, 1)
-    # ypoints = model.predict(xpoints)
+        # output predictions
+        xpoints = np.linspace(np.max(X), np.min(X), 100).reshape(-1, 1)
+        ypoints = model.predict(xpoints)
 
-    # # plot curve
-    # plt.figure()
-    # plt.plot(X, y, 'rx')
-    # plt.title('PolyRegression with d = ' + str(d) + ', lambda = ' + str(reg_lambda))
-    # plt.plot(xpoints, ypoints, 'b-')
-    # plt.xlabel('X')
-    # plt.ylabel('Y')
-    # plt.show()
+        # plot curve
+        plt.figure()
+        plt.plot(X, y, 'rx')
+        plt.title('PolyRegression with d = ' + str(d) + ', lambda = ' + str(reg_lambda))
+        plt.plot(xpoints, ypoints, 'b-')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
 
-    # # show isn't working for me for some reason
-    # plt.savefig("a4_" + str(reg_lambda) + ".png")
+        # show isn't working for me for some reason
+        plt.savefig("a4_" + str(reg_lambda) + ".png")
 
-    reg_lambda_list = [0, 1E-8, 1E-6, 1E-4, 1E-2, 1E-1, 1, 100]
-    plt.figure(figsize=(20, 10), dpi=100)
+    else:
 
-    for idx, reg_lambda in enumerate(reg_lambda_list):
-        plt.subplot(2, 4, idx + 1)
-        plotUnivariate(X, y, d, reg_lambda)
-    
-    plt.savefig("a4_compilation.png")
+        reg_lambda_list = [0, 1E-8, 1E-6, 1E-4, 1E-2, 1E-1, 1, 100]
+        plt.figure(figsize=(20, 10), dpi=100)
+
+        for idx, reg_lambda in enumerate(reg_lambda_list):
+            plt.subplot(2, 4, idx + 1)
+            plotUnivariate(X, y, d, reg_lambda)
+        
+        plt.savefig("a4_compilation.png")
