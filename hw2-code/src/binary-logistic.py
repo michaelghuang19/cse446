c53629707a426176543f846881292ab070397df8
Michael Huang <mhuang19@cs.washington.edu>
2021-04-30 04:21:35 -0700

    issues with final shapes

--- hw2-code/src/binary-logistic.py
+++ hw2-code/src/binary-logistic.py
@@ -31,6 +31,7 @@ class GradientDescent:
     self.lamb = reg_lambda
     self.grad = None
     self.step = step_size
+    self.w = None
     self.b = 0
 
   def grad_desc(self, X, Y, cutoff):
@@ -38,40 +39,71 @@ class GradientDescent:
     
     self.n, self.d = X.shape
     # 12223, 784
-    w_init = np.zeros((self.d, 1))
+    self.w = np.zeros((self.d, 1))
 
-    j_func, grad_w, grad_b = self.get_j(X, Y, w_init, self.b)
+    
+
+    j_func, grad_w, grad_b = self.get_j(X, Y)
 
     print(j_func.shape)
     print(grad_w.shape)
     print(grad_b.shape)
 
-  def get_j(self, X, Y, w, b):
-    offset = b + w.T.dot(X.T)
+    j_data = [j_func[0][0]]
+    class_data = [self.get_error(X, Y)]
+
+    print(j_data)
+    print(class_data)
+
+    # while np.max(np.abs(self.w)) > cutoff:
+      # keep iterating
+
+      # do we add or subtract here?
+      # self.w = self.w + self.step * grad_w
+      # self.b = self.b + self.step * grad_b
+
+      # j_func, grad_w, grad_b = self.get_j(X, Y)
+
+      # j_data.append(j_func)
+      # class_data.append(self.get_error(X, Y))
+
+    # then test on test data
+
+  def get_j(self, X, Y):
+    offset = self.b + (self.w.T).dot(X.T)
     exponent = np.multiply(-Y, offset)
     mu = 1 / (1 + np.exp(exponent))
 
-    j_func = (1 / self.n) * np.log(1 / mu) + self.lamb * (w.T).dot(w)
+    j_func = (1 / self.n) * np.sum(np.log(1 / mu)) + self.lamb * (self.w.T).dot(self.w)
 
     coef = np.multiply(-Y, X.T)
-    grad_w = (1 / self.n) * coef.dot((1 - mu).T) + 2 * self.lamb * w
+    grad_w = (1 / self.n) * coef.dot((1 - mu).T) + 2 * self.lamb * self.w
 
     grad_b = (1 / self.n) * -Y.dot((1 - mu).T)
 
     return j_func, grad_w, grad_b
 
-  def get_error(self, X, Y, w, b):
-    print("calcualte gradient descent error")
+  def get_error(self, X, Y):
+    print("calculate gradient descent error")
 
+    sign = np.sign(self.b + (self.w.T).dot(X.T))
+    sign = sign[0]
 
+    print(Y)
+    print(sign)
+    match_count = np.sum([sign[idx] == val for idx, val in enumerate(Y.T)])
 
-  def plot_objective(self):
+    return 1 - (match_count / self.n)
+
+  def plot_objective(self, train_j, test_j):
     print("plot gradient descent error")
 
     # c.results_path
 
-  def plot_error(self):
+  def plot_error(self, train_error, test_error):
     print("plot gradient descent error")
+
+
 
 
 class StochasticGradientDescent:
