be2fe3b5b12c9d4f81e40ef57849240421dd9c00
Michael Huang <mhuang19@cs.washington.edu>
2021-05-22 13:42:30 -0700

    a6b complete

--- hw3-code/src/graph_models.py
+++ hw3-code/src/graph_models.py
@@ -8,8 +8,8 @@ import numpy as np
 import data as d
 import helpers as h
 
-graph_a = True
-graph_b = False
+graph_a = False
+graph_b = True
 graph_c = False
 graph_d = False
 
@@ -29,20 +29,35 @@ def graph():
       end = grouping[1]
 
       h.plot_acc(d.a_train[start:end], d.a_labels[start:end],
-                 "a6_a_t{}.png".format(idx), "training")
+                 "a6a/a6_a_t{}.png".format(idx), "training")
       h.plot_acc(d.a_valid[start:end], d.a_labels[start:end],
-                 "a6_a_v{}.png".format(idx), "validation")
+                 "a6a/a6_a_v{}.png".format(idx), "validation")
 
     best_idx = 10
     h.plot_acc([d.a_train[best_idx]], [d.a_labels[best_idx]],
-               "a6_a_tb.png", "training best hyperparameters")
+               "a6a/a6_a_tb.png", "training best hyperparameters")
     h.plot_acc([d.a_valid[best_idx]], [d.a_labels[best_idx]],
-               "a6_a_vb.png", "validation best hyperparameters")
+               "a6a/a6_a_vb.png", "validation best hyperparameters")
 
   if graph_b:
     print("graphing b")
 
-    
+    b_groupings = [[0, 6], [6, 12], [12, 18]]
+
+    for idx, grouping in enumerate(b_groupings):
+      start = grouping[0]
+      end = grouping[1]
+
+      h.plot_acc(d.b_train[start:end], d.b_labels[start:end],
+                 "a6b/a6_b_t{}.png".format(idx), "training")
+      h.plot_acc(d.b_valid[start:end], d.b_labels[start:end],
+                 "a6b/a6_b_v{}.png".format(idx), "validation")
+
+    best_idx = 8
+    h.plot_acc([d.b_train[best_idx]], [d.b_labels[best_idx]],
+               "a6b/a6_b_tb.png", "training best hyperparameters")
+    h.plot_acc([d.b_valid[best_idx]], [d.b_labels[best_idx]],
+               "a6b/a6_b_vb.png", "validation best hyperparameters")
 
   if graph_c:
     print("graphing c")
