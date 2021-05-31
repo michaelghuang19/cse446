31f7518a87ff686d7a848a994914df32c4ca2047
Michael Huang <mhuang19@cs.washington.edu>
2021-05-30 18:39:59 -0700

    e complete

--- hw4-code/src/pca.py
+++ hw4-code/src/pca.py
@@ -58,10 +58,29 @@ def part_d(v_list):
     axes_list += list(item)
 
   for i, ax in enumerate(axes_list):
-    img = v_list[i,].reshape((28, 28))
+    img = v_list[:,i].reshape((28, 28))
     ax.imshow(img, cmap='gray')
 
-  fig.savefig(c.results_path + "a6_c")
+  fig.savefig(c.results_path + "a3_d")
+
+def part_e(X_train, v_list):
+  # for 2, 6, 7
+  idx_set = [5, 13, 15]
+  k_set = [5, 15, 40, 100]
+  final_set = [X_train[idx_set[i], :] for i, _ in enumerate(idx_set)]
+
+  for k in k_set:
+    for idx in idx_set:
+      image = (v_list[:, :k]).dot(v_list[:, :k].T)
+      image = image.dot(X_train.T).T
+      final_set.append(image[idx])
+
+  fig, axes = plt.subplots(4, len(idx_set))
+
+  for i, ax in enumerate(axes.ravel()):
+    ax.imshow(final_set[i].reshape((28, 28)), cmap='gray')
+
+  fig.savefig(c.results_path + "a3_e")
 
 def main():
   output = open(c.results_path + "a3.txt", "w")
@@ -75,13 +94,14 @@ def main():
 
   # train_mse_data, test_mse_data, frac_data = part_c(
   #     X_train, X_test)
-
   # h.plot_multiple("error over k", "a3_cerr", "k", "error", 
   #                 [train_mse_data, test_mse_data], c.tt_list)
   # h.plot_multiple("obj over k", "a3_cobj", "k", "obj", 
   #                 [frac_data], ["frac"])
 
-  part_d(v_list) 
+  # part_d(v_list) 
+
+  part_e(X_train, v_list)
 
   output.close()
 
